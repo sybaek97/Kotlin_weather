f44417bc3a7ec437cccd972e51c46f464f374c94
baeksy97 <baeksy1018@gmail.com>
2023-12-10 21:06:46 +0900

    승용 집

--- app/src/main/java/com/crepass/kotlin_weather/GeoPointConverter.kt
+++ app/src/main/java/com/crepass/kotlin_weather/GeoPointConverter.kt
@@ -1,6 +1,13 @@
 package com.crepass.kotlin_weather
 
+import android.graphics.Point
+import android.util.Log
 import kotlin.math.PI
+import kotlin.math.cos
+import kotlin.math.log2
+import kotlin.math.pow
+import kotlin.math.sin
+import kotlin.math.tan
 
 class GeoPointConverter {
 
@@ -19,5 +26,42 @@ class GeoPointConverter {
 
     private val DEGRAD= PI/180.0
     private val RADDEG=180.0/PI
+
+    private val re = RE/GRID
+    private val slat1=SLAT1 * DEGRAD
+    private val slat2=SLAT2*DEGRAD
+    private val olon = OLON * DEGRAD
+    private val olat = OLAT * DEGRAD
+
+    fun convert(lon: Double, lat: Double): Point {
+
+        var sn = tan(PI * 0.25 + slat2 * 0.5) / tan(PI * 0.25 + slat1 * 0.5)
+        sn = log2(cos(slat1) / cos(slat2)) / log2(sn)
+        var sf = tan(PI * 0.25 + slat1 * 0.5)
+        sf = sf.pow(sn) * cos(slat1) / sn
+        var ro = tan(PI * 0.25 + olat * 0.5)
+        ro = re * sf / ro.pow(sn)
+
+        var ra = tan(PI * 0.25 + lat * DEGRAD * 0.5)
+        ra = re * sf / ra.pow(sn)
+
+        var theta = lon * DEGRAD - olon
+
+        if (theta > PI)
+            theta -= 2 * PI
+        if (theta < -PI)
+            theta += 2 * PI
+
+        theta *= sn
+
+        val nx = ra * sin(theta) + XO + 1.5
+        val ny = ro - ra * cos(theta) + YO + 1.5
+
+        Log.e("Converter", "$nx $ny")
+        return Point(nx.toInt(), ny.toInt())
+
+    }
+
+
 
 }
